c04ec847967df7e5ed9abd86892b0609f5b11c37
Rafi5734 <kamrulhasun5734@gmail.com>
2021-08-25 22:36:02 +0600

    allign fix

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-const currencyOne = document.getElementById( "currency-one" );
-const amountOne = document.getElementById( "amount-one" );
-const currencyTwo = document.getElementById( "currency-two" );
-const amountTwo = document.getElementById( "amount-two" );
+const currencyOne = document.getElementById("currency-one");
+const amountOne = document.getElementById("amount-one");
+const currencyTwo = document.getElementById("currency-two");
+const amountTwo = document.getElementById("amount-two");
 
-const swap = document.getElementById( "swap" );
+const swap = document.getElementById("swap");
 
-let rateDiv = document.getElementById( "rate" );
+let rateDiv = document.getElementById("rate");
 
 
-swap.addEventListener( "click", function ()
-{
+swap.addEventListener("click", function() {
     currencyELOne = currencyOne.value;
     currencyELTwo = currencyTwo.value;
 
-    fetch( `https://api.exchangerate-api.com/v4/latest/${ currencyELOne }` )
-        .then( res => res.json() )
-        .then( data =>
-        {
+    fetch(`https://api.exchangerate-api.com/v4/latest/${ currencyELOne }`)
+        .then(res => res.json())
+        .then(data => {
             const rate = data.rates[currencyELTwo];
             rateDiv.innerText = ` 1 ${ currencyELOne } = ${ rate } ${ currencyELTwo}`;
-            
+
             const firstRate = parseFloat(amountOne.value * rate).toFixed(2);
             amountTwo.value = +firstRate;
         })
-} );
-
+});
